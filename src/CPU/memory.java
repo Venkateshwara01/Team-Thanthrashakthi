7f6b569439725044a8fca84909d7ae917ca8a5ec
fidazainaba <fidazainaba7@gmail.com>
2026-09-05 23:12:10 +0530

    Add files via upload

--- src/CPU/memory.java
+++ src/CPU/memory.java
@@ -1,1 +1,20 @@
+package tests.memory;
 
+import memory.ProgramMemory;
+import org.junit.jupiter.api.Test;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MemoryTest {
+
+    @Test
+    public void testProgramMemoryLoading() {
+        ProgramMemory memory = new ProgramMemory();
+        memory.load(Arrays.asList("MOV A, #10", "; comment line", "ADD A, B"));
+        
+        assertEquals(2, memory.size(), "Comment lines should be ignored during loading");
+        assertEquals("MOV", memory.getInstruction(0).getMnemonic());
+        assertEquals("ADD", memory.getInstruction(1).getMnemonic());
+    }
+}
